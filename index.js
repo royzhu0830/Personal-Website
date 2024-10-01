11761deb25e9b3a657ae335fce4574e951f8b8da
royzhu0830 <royzhu456@hotmail.com>
2024-10-01 16:17:45 -0400

    able to activate dark mode

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ var bg = document.getElementById("dark-mode-bg");
 var words = document.getElementsByClassName("dark-mode-word");
 var nav = document.getElementById("dark-mode-nav");
 var activate = document.getElementById("darkactivate")
+let active = 0;
 
 console.log(today.getHours());
+activate.addEventListener("click", function(e){
+    bg.style.backgroundColor = "#4C6079";
+    for(let word of words){
+        word.style.color="white";
+    }
+    nav.classList.remove("nav-light")
+    nav.classList.remove("bg-light")
+    nav.classList.add("nav-dark")
+    nav.classList.add("bg-dark")
+});
 if(today.getHours()> 17){
     bg.style.backgroundColor = "#4C6079";
     for(let word of words){
